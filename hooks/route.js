f208f11e0e0c7928000e16a60e4eb21de7dd2311
Aaron Buachie <aaron.buachiea@gmail.com>
2022-04-06 12:42:25 -0400

    Create routing hook for redirecting users

---
+++ hooks/route.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import { useAuth } from "../contexts/AuthenticationContext";
+
+export function withPublic(Component) {
+  return function WithPublic(props) {
+    const auth = useAuth();
+    const router = useRouter();
+
+    if (auth.currentUser != null) {
+      router.replace("/");
+      return <h2>Loading...</h2>;
+    }
+    return <Component auth={auth} {...props} />;
+  };
+}
+
+export function withProtected(Component) {
+  return function WithProtected(props) {
+    const auth = useAuth();
+    const router = useRouter();
+
+    if (!auth.currentUser || auth.currentUser == null) {
+      router.replace("/signin");
+      return <h2>Loading...</h2>;
+    }
+    return <Component auth={auth} {...props} />;
+  };
+}
