42bdf151849c51d61d6b8623790c9b44de4a41b1
Aaron Buachie <aaron.buachiea@gmail.com>
2022-07-18 00:13:27 -0400

    Add welcome text when user is logged in with no invoices

--- components/Invoice/NoInvoices.js
+++ components/Invoice/NoInvoices.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useAuth } from "../../contexts/AuthenticationContext";
 
 const Wrapper = styled.div`
   display: flex;
@@ -13,14 +14,26 @@ const Wrapper = styled.div`
   }
 `;
 const NoInvoices = () => {
+  const { currentUser } = useAuth();
+
   return (
     <Wrapper>
       <img src="/illustration-empty.svg" alt="empty illustration" />
       <h1>There is nothing here</h1>
-      <p>
-        Create an invoice by clicking the <br />
-        <span className="bold">New Invoice</span> button and get started
-      </p>
+      {currentUser ? (
+        <>
+          <h2>Welcome {currentUser.email}</h2>
+          <p>
+            Create an invoice by clicking the <br />
+            <span className="bold">New Invoice</span> button and get started
+          </p>
+        </>
+      ) : (
+        <p>
+          Create an invoice by clicking the <br />
+          <span className="bold">New Invoice</span> button and get started
+        </p>
+      )}
     </Wrapper>
   );
 };
